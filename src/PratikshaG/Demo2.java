e276b445d4e92d67cb76708cff5d034c4e5dcb83
Pratiksha Gaurkhede <pgaurkhede11@gmail.com>
2024-11-17 13:30:59 +0530

    Pratiksha_Assignment_2_4

---
+++ src/PratikshaG/Demo2.java
@@ -0,0 +1,22 @@
+package PratikshaG;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		
+		String Name;
+		char Grade;
+		int Number;
+		
+		Name= "Automation Testing";
+		Grade='A';
+		Number=22;
+		
+		System.out.println(Name);
+		System.out.println(Grade);
+		System.out.println(Number);
+		
+
+	}
+
+}
---
+++ src/PratikshaG/Demo3.java
@@ -0,0 +1,43 @@
+package PratikshaG;
+
+public class Demo3 {
+
+	public static void main(String[]args) {
+	
+	//syntax-DataType variableName;
+		String InstituteName;
+		String CourseName;
+		String FirstName;
+		String LastName;
+		String MailId;
+		int PassoutYear;
+		int RollNo;
+		char Grade;
+		float StudentPercentage;
+		
+		//VariableName=variableValue;
+		InstituteName="VTCT";
+		CourseName="Software Testing";
+		FirstName="Pratiksha";
+		LastName="Sarode";
+		MailId="psarode11@gmail.com";
+		PassoutYear=2020;
+		RollNo=22;
+		Grade='A';
+		StudentPercentage=80.60f;
+		
+		//printing statement
+		//
+		System.out.println("Institute Name--"+ InstituteName);
+		System.out.println("Course Name--"+ CourseName);
+		System.out.println("First Name--"+ FirstName);
+		System.out.println("Last NAme--"+ LastName);
+		System.out.println("Student MailId--"+ MailId);
+		System.out.println("Student Passout Year--"+ PassoutYear);
+		System.out.println("Student Roll no--"+ RollNo);
+		System.out.println("Student Grade--"+ Grade);
+		System.out.println("Student Percentage--"+ StudentPercentage);
+		
+		
+}
+}
