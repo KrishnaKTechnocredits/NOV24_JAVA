3db1f26dc58732d3cafe10dc55f5f2599857b51f
Krishna Kanani <kananikrishna427@gmail.com>
2024-11-17 13:21:54 +0530

    Krishna_Assignment_3_4

---
+++ src/TechnoCredits/Example4.java
@@ -0,0 +1,17 @@
+package TechnoCredits;
+
+public class Example4 {
+	
+	void processData(String name, int rno) {
+		if(name.equals("Maulik") && rno == 13) {
+			System.out.println("Technocredits");
+		}else {
+			System.out.println("NOV2024");
+		}
+	}
+	
+	public static void main(String[] args) {
+		Example4 ex4 = new Example4();
+		ex4.processData("Maulik", 13);
+	}
+}
