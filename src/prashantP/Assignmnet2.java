b8239c12ae37aa6acb58f837335a4c33f054e0c0
Prashant2020 <pawar7smailbox2.com>
2024-11-17 20:26:39 +0530

    Prashant_Assignment2

---
+++ src/prashantP/Assignmnet2.java
@@ -0,0 +1,37 @@
+package prashantP;
+class Assignmnet2{
+	int num1=10;
+	int num2=2;
+	int addAns;int subAns;int mulAns;int divAns;int finalAns;
+
+	void addation(){
+	  addAns=num1+num2;
+	}
+	
+	void substraction(){
+	  subAns=num1-num2;
+	}
+	
+	void multiplication(){
+	  mulAns=num1*num2;
+	}
+	
+	void division(){
+	  divAns=num1/num2;
+	}
+	
+	void printTotalAns(){
+		finalAns=addAns+subAns+mulAns+divAns;
+		System.out.println("Answer is-:"+finalAns);
+	}
+	
+	public static void main(String[] args){
+		Assignmnet2 assignmnet2=new Assignmnet2();
+		assignmnet2.addation();
+		assignmnet2.substraction();
+		assignmnet2.multiplication();
+		assignmnet2.division();
+		assignmnet2.printTotalAns();
+	}
+
+}
