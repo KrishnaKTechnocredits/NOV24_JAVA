7a26b72170bfc827a818bc8542b9785d0e089367
AK GIT PRACTICE <lakhamapure25@gmail.com>
2024-11-18 02:11:22 +0530

    Aakash_Assignment

---
+++ src/akashL/A3_diff_class.java
@@ -0,0 +1,20 @@
+package akashL;
+
+public class A3_diff_class {//BLC
+	
+public static void m1 () {
+	System.out.println("static regular method calling from different class m1 with parle-G");
+	}
+
+public static void m2() {
+	System.out.println("static regular method calling from different class m2 with good-day");
+}
+
+public static void m3() {
+	System.out.println("static regular method calling from different class m3 with dairy-milk");
+}
+
+public static void m4() {
+	System.out.println("static regular method calling from different class m4 with kit-kat");
+}
+}
