d6f40a44a204cb34ed528e90b069b16a1a0558f4
Prashant2020 <pawar7smailbox2.com>
2024-11-17 21:01:35 +0530

    Prashant_Assignment5

---
+++ src/prashantP/BankDetails.java
@@ -0,0 +1,38 @@
+package prashantP;
+class BankDetails{
+	int balance=1000;
+	int debitCount; 
+	int creditCount;
+	int printStatementCount;
+	
+	void debitAmt(){
+		balance=balance-200;
+		debitCount++;
+	}
+	
+	void creditAmt(){
+		balance=balance+400;
+		creditCount++;
+	}
+	
+	void printStatement(){
+		System.out.println("Your remaining  balance is-:"+balance);	
+		printStatementCount++;
+	}
+	
+	void printCountInfo(){
+		System.out.println("Your debit method call count is-:"+debitCount);
+		System.out.println("Your credit  method call count is-:"+creditCount);
+		System.out.println("Your printstatement method call count is-:"+printStatementCount);
+	}
+	
+	public static void main(String[] args){
+		BankDetails bankdetails=new BankDetails();
+		bankdetails.debitAmt();
+		bankdetails.creditAmt();
+		bankdetails.creditAmt();
+		bankdetails.creditAmt();
+		bankdetails.printStatement();
+		bankdetails.printCountInfo();
+	}	
+}
