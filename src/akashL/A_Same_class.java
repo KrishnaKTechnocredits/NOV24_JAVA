1e491194fa8ae73f56fffbf652c10710a85c59ca
AK GIT PRACTICE <lakhamapure25@gmail.com>
2024-11-18 01:08:44 +0530

    Aakash_Assignment

---
+++ src/akashL/A_Same_class.java
@@ -0,0 +1,28 @@
+package akashL;
+
+public class A_Same_class {
+	public static void main(String[] args) {//main method
+		
+	m1();
+	m2();
+	m3();
+	m4();
+	}
+	
+public static void m1() {
+	System.out.println("static regular method m1 from same class");
+    }
+
+public static void m2() {
+	System.out.println("static regular method m2 from same class");
+    }
+
+public static void m3() {
+		System.out.println("static regular method m3 from same class");
+	}
+	
+public static void m4() {
+	System.out.println("static regular method m4 from same class");
+}
+}
+
