6958d89d7234ee6b97afd58bb372feda03292953
Nishit Ladani <nishitladani2111@gmail.com>
2024-11-18 13:37:32 +0530

    Assignment-4

---
+++ src/NishitP/EmployeeDetails.java
@@ -0,0 +1,62 @@
+package NishitP;
+
+class EmployeeDetails
+{
+	String firstName;
+	String lastName;
+	int employeeID;
+	int managerID;
+	String address;
+	String phoneNumber;
+	
+	void updatePrimaryInfo()
+	{
+		firstName = "Nishit";
+		lastName = "Ladani";
+		employeeID = 1;
+		managerID = 111;
+	}
+	
+	void updateSecondaryInfo()
+	{
+		address = "Rajkot, Gujarat";
+		phoneNumber = "+91-9876543210";
+	}
+	
+	void displayPrimaryInfo()
+	{
+		System.out.println(firstName);
+		System.out.println(lastName);
+		System.out.println(employeeID);
+		System.out.println(managerID);
+		System.out.println("\n");
+	}
+	
+	void displaySecondaryInfo()
+	{
+		System.out.println(address);
+		System.out.println(phoneNumber);
+		System.out.println("\n");
+	}
+	
+	void displayAllInfo()
+	{
+		System.out.println(firstName);
+		System.out.println(lastName);
+		System.out.println(employeeID);
+		System.out.println(managerID);
+		System.out.println(address);
+		System.out.println(phoneNumber);
+		System.out.println("\n");
+	}
+
+	public static void main(String[] args)
+	{
+		EmployeeDetails employeeDetails = new EmployeeDetails();
+		employeeDetails.updatePrimaryInfo();
+		employeeDetails.updateSecondaryInfo();
+		employeeDetails.displayPrimaryInfo();
+		employeeDetails.displaySecondaryInfo();
+		employeeDetails.displayAllInfo();
+	}
+}
