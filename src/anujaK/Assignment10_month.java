ae26a1bf8f112e4e1bb22a118747f46efd6b6f0e
anujak <anuja.kannawar24@gmail.com>
2024-11-19 01:47:46 +0530

    updated changes, modified the code for better readability

--- src/anujaK/Assignment10_month.java
+++ src/anujaK/Assignment10_month.java
@@ -25,17 +25,21 @@ package anujaK;
 
 public class Assignment10_month {
 	public void displayMonthInfo(int month) {
-		//first if block with nested  if else for Q1 and its individual months.
+		//first else if block with nested  if else for Q1 and its individual months.
 		
 		if(month>=1 && month <=3) {
 			if (month==1) {
 				System.out.println("Month index is "+month+" in Q1 , its Jan month");
+				System.out.println("------------------------ \n");
 			}
 			else if (month==2) {
 				System.out.println("Month index is "+month+" in Q1 , its Feb month");
+				System.out.println("------------------------ \n");
+
 				}
 			else if (month==3) {
 				System.out.println("Month index is "+month+" in Q1 , its March month");
+				System.out.println("------------------------ \n");
 				}
 			}
 		
@@ -43,12 +47,15 @@ public class Assignment10_month {
 			//second else if block for Q2 and its individual months.
 			if (month==4) {
 				System.out.println("Month index is "+month+" in Q2 , its Apr month");
+				System.out.println("------------------------ \n");
 			}
 			else if (month==5) {
 				System.out.println("Month index is "+month+" in Q2 , its May month");
+				System.out.println("------------------------ \n");
 				}
 			else if (month==6) {
 				System.out.println("Month index is "+month+" in Q2 , its June month");
+				System.out.println("------------------------ \n");
 				}
 			}
 		
@@ -56,12 +63,15 @@ public class Assignment10_month {
 			//Third else if block for Q2 and its individual months.
 			if (month==7) {
 				System.out.println("Month index is "+month+" in Q3 , its July month");
+				System.out.println("------------------------ \n");
 			}
 			else if (month==8) {
 				System.out.println("Month index is "+month+" in Q3 , its Aug month");
+				System.out.println("------------------------ \n");
 				}
 			else if (month==9) {
 				System.out.println("Month index is "+month+" in Q3 , its Sept month");
+				System.out.println("------------------------ \n");
 				}
 			}
 		
@@ -69,31 +79,35 @@ public class Assignment10_month {
 			//Last else if block for Q4 and its individual months.
 			if (month==10) {
 				System.out.println("Month index is "+month+" in Q4 , its Oct month");
+				System.out.println("------------------------ \n");
 			}
 			else if (month==11) {
 				System.out.println("Month index is "+month+" in Q4 , its Nov month");
+				System.out.println("------------------------ \n");
 				}
 			else if (month==12) {
 				System.out.println("Month index is "+month+" in Q4 , its Dec month");
+				System.out.println("------------------------ \n");
 				}
 			}
 		else if(month<0){
-			System.out.println("invalid month index, it can't be negative");
+			System.out.println("invalid month index, it can't be negative");//check if negative num given
+			System.out.println("------------------------ \n");
 		}
 		
 		else if(month>12){
-			System.out.println("invalid month index, it can't be greater than 12");
+			System.out.println("invalid month index, it can't be greater than 12");//check invalid month num
+			System.out.println("------------------------ \n");
 		}
 		}
 			
 	public static void main(String [] args) {
-		Assignment10_month assignment10_month = new Assignment10_month();
+		Assignment10_month assignment10_month = new Assignment10_month();//object of class Assignment10_month
 		assignment10_month.displayMonthInfo(5);
 		assignment10_month.displayMonthInfo(2);
 		assignment10_month.displayMonthInfo(-3);
 		assignment10_month.displayMonthInfo(13);	
-	}//main method calling one display method	
+	}//main method calling display method	
 }
-	
 		
 		
