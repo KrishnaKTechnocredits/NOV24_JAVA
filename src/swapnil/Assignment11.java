8307ff269dec0361ec5a77d96efb7a10a5f50441
Hole <swapnil.hole@t-systems.com>
2024-11-20 21:35:05 +0530

    swapnil_Assignment_11

---
+++ src/swapnil/Assignment11.java
@@ -0,0 +1,39 @@
+package swapnil;
+
+public class Assignment11 {
+  int num1,num2;
+  
+  void min(int num1,int num2) {
+	  if (num1<num2);
+	  System.out.println(num1+ " is smaller between " +num1+ " and " +num2+".");
+  }
+  void max(int num1,int num2) {
+	  if (num1<num2);
+	  System.out.println(num2+ " is bigger between " +num1+ " and " +num2+".");
+  }
+  public static void main(String[] args) {
+	  Assignment11 assignment11 = new Assignment11();
+	  assignment11.min(10, 17);
+	  assignment11.max(10,17);
+}
+  
+
+}
+
+
+
+
+
+
+
+/* Write 2 methods, one method to find maximum number from 2 numbers,
+other to find minimum number from given 2 numbers.
+sample input :
+num1 : 10
+num2 : 17
+output : 10 is smaller between 10 and 17.
+
+sample input :
+num1 : 10
+num2 : 17
+output : 17 is bigger between 10 and 17.8 */
