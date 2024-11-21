aac35a7e10aa8e010e08e66aa359cc1ae9851d4b
RavindraMule <ravindragmule@gmail.com>
2024-11-21 11:23:44 +0530

    changes added

--- src/ravindra/QuarterIndex.java
+++ src/ravindra/QuarterIndex.java
@@ -1,18 +1,18 @@
 //Assignment_10
 package ravindra;
 public class QuarterIndex {
-	void monthIndex(int monthIndex,String month) {
+	void monthIndex(int monthIndex) {
 	if(monthIndex>=1 && monthIndex<=3) {
-		System.out.println("Month index is " +monthIndex+" in Q1, its "+month);
+		System.out.println("Month index is " +monthIndex+" in Q1, its Febuary.");
 	}
 	else if(monthIndex>=4 && monthIndex<=6) {
-		System.out.println("Month index is "+monthIndex+" in Q2, its  "+month);
+		System.out.println("Month index is "+monthIndex+" in Q2, its May.");
 	}
 	else if(monthIndex>=7 && monthIndex<=9) {
-		System.out.println("Month index is "+monthIndex+" in Q2, its "+month);
+		System.out.println("Month index is "+monthIndex+" in Q2, its ");
 	}
 	else if(monthIndex>=10 && monthIndex<=12) {
-		System.out.println("Month index is "+monthIndex+" in Q2, its "+month);
+		System.out.println("Month index is "+monthIndex+" in Q2, its ");
 	}
 	else if(monthIndex>12) {
 		System.out.println("invalid month index,"+monthIndex+" it can't be greater than 13.");
@@ -24,9 +24,9 @@ public class QuarterIndex {
 	
 	public static void main(String []args) {
 		QuarterIndex quarterIndex=new QuarterIndex();
-		quarterIndex.monthIndex(5,"May");
-		quarterIndex.monthIndex(2, "Febuary");
-		quarterIndex.monthIndex(-3, "");
-		quarterIndex.monthIndex(13, "");
+		quarterIndex.monthIndex(5);
+		quarterIndex.monthIndex(2);
+		quarterIndex.monthIndex(-3);
+		quarterIndex.monthIndex(13);
 	}
 }
