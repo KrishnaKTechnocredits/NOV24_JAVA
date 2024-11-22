dc2c7bc930a5602949b9dbcb539add49514eb56b
Reshmabhosale <reshmabhosale4141@.com>
2024-11-22 20:58:48 +0530

    changes incorporated

--- src/reshma/Assignment11_MinMaxNum.java
+++ src/reshma/Assignment11_MinMaxNum.java
@@ -4,13 +4,15 @@ public class Assignment11_MinMaxNum {
 	void m1(int num1, int num2){
 		if (num1<num2){
 		System.out.println( num1+  " is smaller between "+num1+" and "  +num2 );
-	}
+	}else 
+		System.out.println( num2+  " is smaller between "+num1+" and "  +num2 );
 	}
 		
 	void m2(int num1, int num2){
 		if (num1<num2){
 		System.out.println( num2+  " is bigger between "+num1+" and " +num2 );
-	}
+	}else
+		System.out.println( num1+  " is bigger between "+num1+" and " +num2 );
 	}
 		
 	public static void main(String[] args) {
