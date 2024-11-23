1405019b227b3a8c140963128472a3b22f427b94
sindhu-windows <sindhupatil32@gmail.com>
2024-11-23 18:29:37 +0530

    Update MinMaxNumbers.java
    As per your suggestion, I have made changes in the code.
    Please review the code once

--- src/sindhu/MinMaxNumbers.java
+++ src/sindhu/MinMaxNumbers.java
@@ -4,18 +4,14 @@ public class MinMaxNumbers {
 	void maxNum(int a,int b){
 		if(b>a){
 			System.out.println(b+" is bigger between "+a+" and "+b);
-		}
-	}
-	
-	void minNum(int a,int b){
-		if(a<b){
-			System.out.println(a+" is smaller between "+a+" and "+b);
+		} 
+		else{
+			System.out.println(a+" is smalleer between "+a+" and "+b);
 		}
 	}
 	
 	public static void main(String[] args){
 		MinMaxNumbers numbers = new MinMaxNumbers();
 		numbers.maxNum(10,17);
-		numbers.minNum(10,17);
 	}
 }
