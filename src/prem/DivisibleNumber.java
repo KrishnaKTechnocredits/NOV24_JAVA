967ea969d02589efb382f9692a1bd996db2ae1e4
Prem Lata Gupta <premlata.gupta5@gmail.com>
2024-11-23 08:10:45 +0530

    Assignment13_prem

---
+++ src/prem/DivisibleNumber.java
@@ -0,0 +1,18 @@
+package prem;
+
+public class DivisibleNumber {
+	
+	void divisible(int startRange,int endRange) {
+		for(int num = startRange;num <= endRange;num++) {
+		if (num%5==0 && num %3==0) {
+		System.out.println(num);
+		}
+	  }
+	}
+	
+	public static void main(String[] args){
+		DivisibleNumber divisibleNumber = new DivisibleNumber();
+		divisibleNumber.divisible(5,40);
+	}
+
+}
