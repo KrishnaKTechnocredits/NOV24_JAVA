ccf9d1db6029dfd6cfdff2d42ea7f7d898ab7b1d
Harshada JagtapW <jagtap.harshada.11@gmail.com>
2024-11-23 12:25:10 +0530

    Harshada_Assignment11

---
+++ src/harshada/MaxMinNumber.java
@@ -0,0 +1,40 @@
+/**
+ * Assignment 11 - Program to print greater and smaller numbers between two
+ * numbers
+ * Write 2 methods, one method to find maximum number from 2 numbers,
+other to find minimum number from given 2 numbers.
+sample input :
+num1 : 10
+num2 : 17
+output : 10 is smaller between 10 and 17.
+
+sample input :
+num1 : 10
+num2 : 17
+output : 17 is bigger between 10 and 17.
+ */
+
+package harshada;
+
+public class MaxMinNumber {
+	
+	void findMaxNumber(int num1, int num2) {
+		if(num1>num2)
+			System.out.println(num1+" is bigger between "+num1+" and "+num2);
+		else
+			System.out.println(num2+" is bigger between "+num1+" and "+num2);
+	}
+	
+	void findMinNumber(int num1, int num2) {
+		if(num1<num2)
+			System.out.println(num1+" is smaller between "+num1+" and "+num2);
+		else
+			System.out.println(num2+" is smaller between "+num1+" and "+num2);
+	}
+	
+	public static void main(String[] args) {
+		MaxMinNumber maxMinNumber= new MaxMinNumber();
+		maxMinNumber.findMinNumber(10, 17);
+		maxMinNumber.findMaxNumber(10, 17);
+	}
+}
