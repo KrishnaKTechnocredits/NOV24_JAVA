4de06291764a7d782997bb08b4dbb10455746429
anujak <anuja.kannawar24@gmail.com>
2024-11-25 01:11:06 +0530

    created new package under originalpackage and added 3 assignments form date 24th on string

---
+++ src/anuja/string/Assignment20.java
@@ -0,0 +1,24 @@
+package anuja.string;
+/*Find the given character count from given string.*/
+public class Assignment20 {
+	//method to print the count of matched char, use equals and run for loop to iterate
+	int getCharFreq(String str, char ch) {
+		int count=0;
+		int stringLength = str.length();
+		for(int index =0;index<=stringLength-1;index++) {
+			if(str.charAt(index)==(ch)) {
+				count++;
+			}
+		}
+		System.out.println("Count of " +ch+ " char in string "+str+" is "+count+" .\n");
+		return count;
+	}
+	
+	public static void main(String[] args) {
+		Assignment20 assignment20 = new Assignment20();
+		assignment20.getCharFreq("aakanskha" , 'a');
+		
+		
+	}
+
+}
