9d8180cd2b53025f32aac75219a308c123cd480f
Reshmabhosale <reshmabhosale4141@.com>
2024-11-26 23:17:59 +0530

    code changes as per PR comment

--- src/reshma/Assignment10_MonthDetails.java
+++ src/reshma/Assignment10_MonthDetails.java
@@ -2,36 +2,40 @@ package reshma;
 
 public class Assignment10_MonthDetails {
 	void getMonth(int index){
-		if(index==1){
+		if(index<1){
+			System.out.println(" invalid month index, it can't be less than 1.");
+		}else if(index>=1 && index<=3) {
+			if(index==1){
 			System.out.println(" Month index " + index + " is in Q1, its January.");
 			}else if(index==2){
 				System.out.println(" Month index " + index + " is in Q1, its February.");
-			}else if(index==3){
+			}else
 				System.out.println(" Month index " + index + " is in Q1, its March.");
-			}else if(index==4) {
+		}else if(index>=4 && index<=6) {
+				if(index==4) {
 				System.out.println(" Month index " + index + " is in Q1, its April.");
 			}else if(index==5) {
 				System.out.println(" Month index " + index + " is in Q2, its May.");
-			}else if(index==6) {
+			}else 
 				System.out.println(" Month index " + index + " is in Q2, its Jun.");
-			}else if(index==7) {
+		}else if(index>=7 && index<=9) {
+			if(index==7) {
 				System.out.println(" Month index " + index + " is in Q2, its July.");
 			}else if(index==8) {
 				System.out.println(" Month index " + index + " is in Q2, its August.");
-			}else if(index==9) {
+			}else 
 				System.out.println(" Month index " + index + " is in Q3, its September.");
-			}else if(index==10) {
+		}else if(index>=10 && index<=12) {
+					if(index==10) {
 				System.out.println(" Month index " + index + " is in Q3, its October.");
 			}else if(index==11) {
 				System.out.println(" Month index " + index + " is in Q1, its November.");
-			}else if(index==12) {
+			}else 
 				System.out.println(" Month index " + index + " is in Q1, its December.");
-			}else if(index<1){
-				System.out.println(" invalid month index, it can't be negative.");
-			}else
+		}else 
 				System.out.println(" invalid month index, it can't be greater than " +index);
-
-	}
+		}
+	
 
 			public static void main(String[]args){
 				
