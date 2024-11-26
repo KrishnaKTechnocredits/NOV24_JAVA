2d225bad88d9f3a597205f3479e571dc29c33f97
Reshmabhosale <reshmabhosale4141@.com>
2024-11-26 22:58:31 +0530

    code changes as per PR comment

--- src/reshma/Assignment13_DivByNum.java
+++ src/reshma/Assignment13_DivByNum.java
@@ -2,9 +2,9 @@ package reshma;
 
 public class Assignment13_DivByNum {
 	void printDivByNum(int startindex,int endindex){
-		System.out.println("Divisible by 5 & 3, numbers are: ");
-	for (int i=05;i<=40;i++){
-	if(i%5==0 && i%3==0){
+		System.out.println("Divisible by 3 & 5, numbers are: ");
+	for (int i=startindex;i<=endindex;i++){
+	if(i%3==0 && i%5==0){
 	System.out.println(i);
 	}
 	}
@@ -12,6 +12,6 @@ public class Assignment13_DivByNum {
 	
 	public static void main(String[] args) {
 		Assignment13_DivByNum d=new Assignment13_DivByNum();
-		d.printDivByNum(10,15);
+		d.printDivByNum(10,40);
 	}
 }
