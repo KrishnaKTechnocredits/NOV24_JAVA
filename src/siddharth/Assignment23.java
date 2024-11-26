eb5f82c3736be5ad06dc98de7b388973954eb493
Siddharth Kale <Siddharth.kale@ubisoft.com>
2024-11-26 22:59:07 +0530

    Siddharth_Assignment23

---
+++ src/siddharth/Assignment23.java
@@ -0,0 +1,39 @@
+package siddharth;
+
+public class Assignment23 {
+
+	void printCount(String str) {
+		int dcount = 0;
+		int ucount = 0;
+		int lcount = 0;
+		int spacecount = 0;
+		int scount = 0;
+		
+		for (int index = 0; index < str.length(); index++) {
+			char ch = str.charAt(index);
+			if (Character.isLetter(ch)) {
+				if (Character.isUpperCase(ch)) {
+					ucount++;
+				} else {
+					lcount++;
+				}
+			} else if (Character.isDigit(ch)) {
+				dcount++;
+			} else if (ch == ' ') {
+				spacecount++;
+				
+			} else {
+				scount++;
+			}
+		}
+		System.out.println("Output : uppercase -> " + ucount);
+		System.out.println("lowerrcase -> " + lcount);
+		System.out.println("digit -> " + dcount);
+		System.out.println("space -> " + spacecount);
+		System.out.println("special char -> " + scount);
+	}
+
+	public static void main(String[] args) {
+		new Assignment23().printCount("Te4c_hno Credi3ts");
+	}
+}
