345b780f8c0ba736cb86b5a8b6372b70aac0d55d
sindhu-windows <sindhupatil32@gmail.com>
2024-11-28 19:29:48 +0530

    Update Divisible5Last.java
    Please review the updated code.

--- src/sindhu/Divisible5Last.java
+++ src/sindhu/Divisible5Last.java
@@ -2,7 +2,7 @@ package sindhu;
 
 public class Divisible5Last {
 	int lastNum= 0;
-	int getLastDivNumber(int endIndex, int startIndex) {
+	int getLastDivNumber(int startIndex, int endIndex) {
 		for (int i = endIndex; i >= startIndex; i--) {
 			if (i % 5 == 0) {
 			    lastNum = i;
@@ -14,7 +14,12 @@ public class Divisible5Last {
 
 	public static void main(String[] args) {
 		Divisible5Last divisible5Last = new Divisible5Last();
-		int lastNumber = divisible5Last.getLastDivNumber(498, 10);
+		int lastNumber = divisible5Last.getLastDivNumber(10, 498);
 		System.out.println(lastNumber+" is the last number which is div by 5 in the given range.");
+		if (lastNumber == 0) {
+			System.out.println("There is no such number which is divisible by 5 in the given range.");
+		} else {
+			System.out.println(lastNumber + " is the last number which is divisible by 5 in the given range.");
+		}
 	}
 }
