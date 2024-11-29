23b4825e0a17f5a857c8449f55c162403f2f117d
Hole <swapnil.hole@t-systems.com>
2024-11-29 23:11:45 +0530

    Swapnil_Assignment_24

---
+++ src/technocredits/Employee.java
@@ -0,0 +1,13 @@
+package technocredits;
+
+public class Employee extends Manager {
+	int y = 20;
+	int z  = 30;
+	
+	void m2 () {
+		System.out.println("Employee m2");
+	}
+	void m3 () {
+		System.out.println("Employee m3");
+	}
+}
