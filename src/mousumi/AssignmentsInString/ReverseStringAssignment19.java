5252d5c61e4451972893680a3325c320bee68817
MousamiB <mousumibanji11@gmail.com>
2024-11-29 22:54:11 +0530

    Mousumi_ReverseStringAssignment19

---
+++ src/mousumi/AssignmentsInString/ReverseStringAssignment19.java
@@ -0,0 +1,29 @@
+package mousumi.AssignmentsInString;
+/**
+ * 
+ Assignment - 19 : 24rd Nov'2024 [20 mins]
+Print all the characters of given string in reverse order. 
+
+input : te ch3no
+output : o
+         n
+		 3
+		 h
+         c
+		 
+         e 
+         t  		 
+ */
+	
+	public class ReverseStringAssignment19 {
+		static void printReverseCharacters(String input) {
+		        for (int i = input.length() - 1; i >= 0; i--) {
+		            System.out.println(input.charAt(i)); 
+		        }
+		    }
+		
+	     public static void main(String[] args) {
+		        String input = "te ch3no";
+	            printReverseCharacters(input);
+		    }
+		}
