a2a64193467798792875cdea810a30c3556439b9
Sachin Bhoite <sachinbhoite130@gmail.com>
2024-11-29 20:40:51 +0530

    sachin_Assignment-16

---
+++ src/sachin/Assignment16.java
@@ -0,0 +1,32 @@
+package sachin;
+
+
+
+public class Assignment16 {
+	
+		static int count;
+		static int sum;
+		
+		int getRangeSum(int startRange,int endRange) {
+			for(int num=startRange;num<=endRange;num++) {
+				if (num%5==0 && num%7==0) {
+					count++;
+					sum= sum +num;
+					if(count>=3) {
+						return sum;
+					}
+				}
+			}
+			return 0;
+		}
+		
+		public static void main(String[] args) {
+			Assignment16 assign16 = new Assignment16();
+			int ans= assign16.getRangeSum(1,210);
+	  		int average = ans /count;
+			System.out.println("sum of 3 divisible numbers by 5 & 7 are :" +ans);
+			System.out.println("Average of 3 divible numbers are :"+ average);
+		}
+
+	}
+
