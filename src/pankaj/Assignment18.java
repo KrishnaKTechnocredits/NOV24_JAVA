8ab8419fd3c28b9b42fbbd582dcab7e02378d9f7
Pankaj Patil <pankajp0099gmail.com>
2024-11-29 18:12:54 +0530

    Pankaj_Assignmnet18_Changes Done

--- src/pankaj/Assignment18.java
+++ src/pankaj/Assignment18.java
@@ -10,7 +10,7 @@ public class Assignment18 {
 		int num=0;
 		int count=0;
 
-		for (num = endrange; num >= startrange; num--) 
+		for (num = startrange; num >=endrange ; num--) 
 		{
 			if (num % 5 == 0) 
 			{
@@ -34,7 +34,7 @@ public class Assignment18 {
 	public static void main(String[] args) {
 
 		Assignment18 A18 = new Assignment18();
-		int a = A18.processData(498, 1);
+		int a = A18.processData(10, 498);
 		System.out.println(a + " is the last number divisible by 5");
 	}
 
