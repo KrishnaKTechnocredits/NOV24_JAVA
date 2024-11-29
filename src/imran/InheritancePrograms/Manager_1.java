f23269fea38a2b9593086ea9da5fde60dcd39fb5
Imran Ansari <imranahmedansari8987@gmail.com>
2024-11-29 16:19:12 +0530

    Assignment24 -Inheritance Cases Submitted in Branch ImranAssignment24

---
+++ src/imran/InheritancePrograms/Manager_1.java
@@ -0,0 +1,19 @@
+package imran.InheritancePrograms;
+
+public class Manager_1 {
+
+	int a=10;
+	int b=20;
+	int z;
+	
+	void add()
+	{
+		z=a+b;
+		System.out.println("The addition done by Manager Class (Parent class)is "+ z);
+	}
+	
+	void m2()
+	{
+		System.out.println("This is Manager_1 class m2 Method");
+	}
+}
---
+++ src/imran/InheritancePrograms/Test_Case4.java
@@ -0,0 +1,18 @@
+// Inheritance, Case : 4 
+
+package imran.InheritancePrograms;
+
+public class Test_Case4 {
+
+	public static void main(String[] args) {
+		
+		//Employee_1 E_1 = new Manager_1(); Child class Reference type cannot be created with Parent class object. Compiler will only Consider Parent Class REference or class of Big Reference on the left side.
+		/* System.out.println(E_1.a);
+		System.out.println(E_1.b);
+		System.out.println(E_1.c);
+		E_1.add();//
+		E_1.m2();
+		E_1.m3(); */
+		// all the above code will not be executed as Compiler error on line no:7.Child class Reference type cannot be created with Parent class object. Compiler will only Consider Paint Class REference or class of Big Reference on the left side.
+	}
+}
