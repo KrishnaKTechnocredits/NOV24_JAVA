41d877b4217d76cf882a8c73e51c06f2670e2e1a
Prem Lata Gupta <premlata.gupta5@gmail.com>
2024-11-29 08:33:26 +0530

    Corrected_System.out.print_statementof_else_statement

--- src/prem/MaxMinNumber.java
+++ src/prem/MaxMinNumber.java
@@ -6,14 +6,14 @@ public class MaxMinNumber {
 		if (num1<num2) {
 			System.out.println(num1 + " is smaller between "+ num1 +" and " + num2);
 		}else {
-			System.out.println(num2 + " is bigger between "+ num1 +" and " + num2);
+			System.out.println(num2 + " is smaller between "+ num1 +" and " + num2);
 		}
 	}
 	void max(int num1, int num2) {
 		if (num1> num2) {
 			System.out.println(num1 + " is bigger between "+ num1 +" and " + num2);
 		}else {
-			System.out.println(num2 + " is smaller between "+ num1 +" and " + num2);
+			System.out.println(num2 + " is bigger between "+ num1 +" and " + num2);
 		}		
 	}
 	
