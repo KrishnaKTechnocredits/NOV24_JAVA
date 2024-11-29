e5adaa777f00bfd25cdc082b400be4b31b0d5b96
Sachin Bhoite <sachinbhoite130@gmail.com>
2024-11-29 21:53:53 +0530

    sachin_Assignment-17

---
+++ src/sachin/Assignment17.java
@@ -0,0 +1,26 @@
+package sachin;
+
+public class Assignment17 {
+		
+		void lastNumber(int start, int end) {
+			int count=0;
+			System.out.println("Last five numbers divisible by 5 is: ");
+			for (;start<=end;end--) {
+				if (end%5==0) {
+					System.out.println(end);
+					count++;
+					if (count==5) {
+						break;
+					}
+				}
+			}
+		}
+		
+		public static void main(String[] args) {
+			new Assignment17().lastNumber(10, 500);
+		}
+	
+	
+	
+	
+}
