f94ccaf2e8c5ca55b4bbf924034a498f16dcd188
Nitesh Shukla <nitesh.shukla18@gmail.com>
2024-11-30 12:22:41 +0530

    src/nitesh/PrintDivisible.java

---
+++ src/nitesh/PrintDivisible.java
@@ -0,0 +1,16 @@
+package nitesh;
+
+public class PrintDivisible {
+	
+	void printDiv(int startrange,int endrange) {
+		for(int index =startrange;index<endrange;index++) {
+			if (index%3==0 && index%5==0) {
+				System.out.println("Divisible by 5 & 3, numbers are:"+" "+index);
+			}
+		} 
+	}
+	public static void main(String[] args) {
+		PrintDivisible printdivisible = new PrintDivisible();
+		printdivisible.printDiv(5, 40);
+	}
+}
