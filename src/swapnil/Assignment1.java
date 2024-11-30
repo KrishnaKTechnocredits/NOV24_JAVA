6486f796cbcc5e640af6cb8bb06bdf970cadee50
Hole <swapnil.hole@t-systems.com>
2024-11-30 11:56:56 +0530

    Swapnil_Assignment_1

---
+++ src/swapnil/Assignment1.java
@@ -0,0 +1,17 @@
+package swapnil;
+
+public class Assignment1 {
+	    String name = "credits";
+	    int rno = 1;
+
+	    void printInfo(){
+	        System.out.println(name);
+	        System.out.println(rno);
+	    }
+		
+		public static void main(String[] args){
+			Assignment1 assignment1 = new Assignment1();
+			assignment1.printInfo();
+		}
+	} 
+
