9241c0b84340dcb1d22fc41f10a85f55c02a7f28
Nitesh Shukla <nitesh.shukla18@gmail.com>
2024-11-30 17:37:53 +0530

    src/nitesh/LastFiveNumberDiv.java

---
+++ src/nitesh/LastFiveNumberDiv.java
@@ -0,0 +1,35 @@
+/*Assignment - 17 : 22nd Nov'2024 [20 mins]
+On user defined range print last 5 numbers which is divisible by 5.
+
+Hint : pass start and end index as a parameter.
+
+Input : Range -> 10 to 500
+Output : Last 5 numbers divisible by 5 is :
+500
+495
+490
+485
+480
+ */
+
+package nitesh;
+
+public class LastFiveNumberDiv {
+	int count;
+	
+	void divisibleByFive(int startrange,int endrange) {
+		for(;endrange>=startrange;endrange--) {
+			if(endrange%5==0) {
+				System.out.println("Last 5 numbers divisible by 5 is :"+" "+endrange);
+				count++;
+				if(count==5) {
+					break;
+				} 
+			}
+		}
+	}
+	public static void main(String[] args) {
+		LastFiveNumberDiv lastfivenumberdiv =new LastFiveNumberDiv();
+		lastfivenumberdiv.divisibleByFive(10, 500);
+	}
+}
