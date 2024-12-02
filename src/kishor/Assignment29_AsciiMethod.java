3d1911d26c2a8a01744bf6008d07f2b72928cd76
Dipak Rahangdale <110284680+8408989979@users.noreply.github.com>
2024-12-02 22:27:12 +0530

    update the assignment 29 file

--- src/kishor/Assignment29_AsciiMethod.java
+++ src/kishor/Assignment29_AsciiMethod.java
@@ -1,32 +1,32 @@
 package kishor;
 
 public class Assignment29_AsciiMethod {
-	
-	void asciiUpperCase(char start,char end) {
-		for(char ch=start;ch<=end;ch++) {
+
+	void asciiUpperCase(char start, char end) {
+		for (char ch = start; ch <= end; ch++) {
 			int value = ch;
-			System.out.println("The ascii value for given character "+ch+" is:-"+value);
+			System.out.println("The ascii value for given character " + ch + " is:-" + value);
 		}
 	}
-	
-	void asciiLowerCase(char start,char end) {
-		for(char ch=start;ch<=end;ch++) {
-			int value=ch;
-			System.out.println("The ascii value for given character "+ch+" is:-"+value);
+
+	void asciiLowerCase(char start, char end) {
+		for (char ch = start; ch <= end; ch++) {
+			int value = ch;
+			System.out.println("The ascii value for given character " + ch + " is:-" + value);
 		}
 	}
-	
-	void asciiDigit(int start,int end) {
-		for(int i=start;i<=end;i++) {
-			char ch=(char)i; //explicit type cast
-			System.out.println("The ascii character of given value is "+i+" is:-"+ch);
+
+	void asciiDigit(char start, char end) {
+		for (char i = start; i <= end; i++) {
+			int ascii = i;
+			System.out.println("The ascii value of given character " + i + " is:-" + ascii);
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		Assignment29_AsciiMethod assignment29 = new Assignment29_AsciiMethod();
 		assignment29.asciiUpperCase('A', 'Z');
 		assignment29.asciiLowerCase('a', 'z');
-		assignment29.asciiDigit(0,9);
+		assignment29.asciiDigit('0', '9');
 	}
 }
