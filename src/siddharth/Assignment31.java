ff03c2cfe90c3035cec8a9a95659363d8c7bd716
Siddharth Kale <siddharth.kale@ubisoft.com>
2024-12-03 20:51:09 +0530

    Siddharth_Assignment29_31~

---
+++ src/siddharth/Assignment31.java
@@ -0,0 +1,32 @@
+package siddharth;
+
+//return the difference of Sum of all the odd digits and even digits present in a given String. 
+//HINT : sum of odd digits - sum of even digits
+public class Assignment31 {
+
+	int getSumDiff(String str) {
+		int sumEven = 0;
+		int sumOdd = 0;
+		System.out.println("Input is : "+str);
+		for (int index = 0; index < str.length(); index++) {
+			char ch = str.charAt(index);
+			if (Character.isDigit(ch)) {
+				int num = Character.getNumericValue(ch);
+				if (num % 2 == 0) {
+					sumEven = sumEven + num;
+				} else {
+					sumOdd = sumOdd + num;
+				}
+			}
+		}
+		return sumOdd - sumEven;
+	}
+
+	public static void main(String[] args) {
+		Assignment31 assignment31 = new Assignment31();
+		int ans1 = assignment31.getSumDiff("te1ch2no3cre7dits");
+		int ans2 = assignment31.getSumDiff("4563");
+		System.out.println("Output is : "+ans1);
+		System.out.println("Output is : "+ans2);
+	}
+}
