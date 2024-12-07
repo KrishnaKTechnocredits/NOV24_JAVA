eb48de4194c796c492fec606557b56a37fcd69a7
Harish Rahangdale <hsr.29978@gmail.com>
2024-12-07 08:51:11 +0530

    [Harish][Assignment-33] Print frequency of all upper case character in string

---
+++ src/harish/stringdemo/Assignment33.java
@@ -0,0 +1,29 @@
+package harish.stringdemo;
+
+public class Assignment33 {
+
+	int getCharCount(String str, char ch) {
+		int count = 0;
+		for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) == ch) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	void printFrequencyOfUpperCaseChars(String str) {
+		for (int i = 0; i < str.length(); i++) {
+			if (str.indexOf(str.charAt(i)) == i && Character.isUpperCase(str.charAt(i))) {
+				System.out.println("Frequency of char " + str.charAt(i) + " is - " + getCharCount(str, str.charAt(i)));
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		Assignment33 assignment33 = new Assignment33();
+		String input = "AABaaccDFeABBCCcDdEs";
+		System.out.println("Frequency Map for upper case characters in given string " + input + " is as below-");
+		assignment33.printFrequencyOfUpperCaseChars(input);
+	}
+}
