cfbb870602ebcc87d95fd7733d0af1a62e2bd6d7
Harish Rahangdale <hsr.29978@gmail.com>
2024-12-07 08:34:32 +0530

    [Harish][Assignment-32] Print frequency of each character in string

---
+++ src/harish/stringdemo/Assignment32.java
@@ -0,0 +1,33 @@
+package harish.stringdemo;
+
+/**
+ * Assignment 32 - 7 Dec
+ * 
+ * Find the frequency of each character in a given string
+ */
+
+public class Assignment32 {
+
+	int getCharCount(String str, char ch) {
+		int count = 0;
+		for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) == ch) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	void printFrequencyOfChars(String str) {
+		for (int i = 0; i < str.length(); i++) {
+			if (str.indexOf(str.charAt(i)) == i) {
+				System.out.println("Frequency of char " + str.charAt(i) + " is - " + getCharCount(str, str.charAt(i)));
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		Assignment32 assignment32 = new Assignment32();
+		assignment32.printFrequencyOfChars("technocredits");
+	}
+}
