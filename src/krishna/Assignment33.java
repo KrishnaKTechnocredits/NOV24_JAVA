f57c261c03c7750a7dc105ac6d2291e0c7d3421a
krishnashray2002 <krishnashraytrivedi447@gmail.com>
2024-12-08 00:20:08 +0530

    Krishna_Assignment33 with two different approaches

--- src/krishna/Assignment33.java
+++ src/krishna/Assignment33.java
@@ -1,6 +1,28 @@
 package krishna;
 
+import AssignmentDemoCode.Demo_Assignment33;
+
 public class Assignment33 {
+
+	int getCharFreq(String str , char ch) {
+		int charCount= 0;
+		for(int i = 0; i<str.length();i++) {
+			if(str.charAt(i)== ch) {
+				charCount++;
+			}
+		}
+		return charCount;
+	}
+	
+	void printEachCharFreq(String str) {
+		for(int i = 0;i<str.length();i++) {
+			if(str.indexOf(str.charAt(i)) == i) {
+				if(Character.isUpperCase(str.charAt(i))) {
+					System.out.println("freq of char "+ str.charAt(i) + " in the given string is : " + getCharFreq(str, str.charAt(i)));
+				}
+			}
+		}
+	}
 
 	void processData(String str) {
 		int charCount = 0;
@@ -21,6 +43,9 @@ public class Assignment33 {
 	}
 
 	public static void main(String[] args) {
-		new Assignment33().processData("AAkaNSha");
+		Assignment33 demo = new Assignment33();
+		demo.printEachCharFreq("AABcBBBAdeddd");
+		System.out.println();
+		demo.processData("AABcBBBAdeddd");
 	}
 }
