2bb46cd44ec80202f958739a061975090c7600e9
Hole <swapnil.hole@t-systems.com>
2024-12-08 17:39:17 +0530

    Swapnil_Assignment_44

--- src/swapnil/Assignment44.java
+++ src/swapnil/Assignment44.java
@@ -4,8 +4,8 @@ public class Assignment44 {
 	void pattern() {
 		
 		for(int i=1;i<=4;i++) {
-			for(int j=0;j<=i;j++) {
-				if(j%2==0) {
+			for(int j=1;j<=i;j++) {
+				if(j%2!=0) {
 					System.out.print(" * ");	
 				}else {
 					System.out.print(" # ");
