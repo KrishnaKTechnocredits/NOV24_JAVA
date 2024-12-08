a37af52008a1758e060bea7d07c4e0d3157f246c
RohanKhochare <RDKhochare@outlook.com>
2024-12-09 03:00:42 +0530

    code modified

--- src/rohan/Assignment32_1.java
+++ src/rohan/Assignment32_1.java
@@ -1,20 +1,24 @@
 package rohan;
 
 public class Assignment32_1 {
-	
-void CountCharactor(String input){
-		
-		for(int i=0;i<input.length();i++){
-			int count=0;
-			for(int j=0;j<input.length();j++){
-				if(input.charAt(i)==input.charAt(j)){
-					count++;	
+
+	void CountCharactor(String input) {
+
+		for (int i = 0; i < input.length(); i++) {
+			int count = 0;
+			if (input.indexOf(input.charAt(i)) == i) {
+				for (int j = 0; j < input.length(); j++) {
+					if (input.charAt(i) == input.charAt(j)) {
+						count++;
+					}
 				}
+				System.out.println(input.charAt(i) + "=" + count);
+
 			}
-			System.out.println(input.charAt(i)+"="+count);
 		}
 	}
-	public static void main(String[] args){
+
+	public static void main(String[] args) {
 		Assignment32_1 assignment = new Assignment32_1();
 		assignment.CountCharactor("technocredits");
 	}
