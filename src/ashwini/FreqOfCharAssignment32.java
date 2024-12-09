e85557b75a52ebbc0695031ead74cfaa2d9483a4
Ashwini Bhuskat <ashwini.bhuskat@gmail.com>
2024-12-09 17:31:41 +0530

    Ashwini_Assignment32

--- src/ashwini/FreqOfCharAssignment32.java
+++ src/ashwini/FreqOfCharAssignment32.java
@@ -1,20 +1,18 @@
 package ashwini;
 
 public class FreqOfCharAssignment32 {
-	
+
 	void freqOfChar(String str) {
-		for(int i =0; i < str.length();i++)
-		{
-			char ch=str.charAt(i);
-			int index=str.indexOf(ch);
-			System.out.println(ch+"-->"+i + "-->"+index);	
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			int index = str.indexOf(ch);
+			System.out.println(ch + "-->" + i + "-->" + index);
 		}
 	}
 
 	public static void main(String[] args) {
-		FreqOfCharAssignment32 assn32= new FreqOfCharAssignment32();
+		FreqOfCharAssignment32 assn32 = new FreqOfCharAssignment32();
 		assn32.freqOfChar("aakanksha");
-
 	}
 
 }
