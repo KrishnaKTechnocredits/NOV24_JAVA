34beb57101730cf1edc4fa03b5212126a14ad12f
MousamiB <mousumibanji11@gmail.com>
2024-12-09 22:27:34 +0530

    MousumiB_Assignment19

--- src/mousumi/AssignmentsInString/ReverseStringAssignment19.java
+++ src/mousumi/AssignmentsInString/ReverseStringAssignment19.java
@@ -15,7 +15,7 @@ output : o
          t  		 
  */
 	
-	public class c {
+	public class ReverseStringAssignment19 {
 		static void printReverseCharacters(String input) {
 		        for (int i = input.length() - 1; i >= 0; i--) {
 		            System.out.println(input.charAt(i)); 
