3405d0fcbfac8bbcc2930bd3728ac9eae965d762
Reshmabhosale <reshmabhosale4141@.com>
2024-12-09 23:00:25 +0530

    Reshma_Assignment38

---
+++ src/reshma/Assignment38.java
@@ -0,0 +1,22 @@
+package reshma;
+/* write a logic to print below pattern.
+
+*
+**
+***
+****
+-----------------------------
+*/
+
+public class Assignment38 {
+	public static void main(String[] args) {
+		for(int i=1;i<=4;i++) {
+			for(int j=1;j<=i;j++) {
+				
+				System.out.print( " * " );
+			}
+			System.out.println();
+		}
+		
+	}
+}
