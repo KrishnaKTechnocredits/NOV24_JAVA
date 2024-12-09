451078e0c3b3a25c2c02e7666e2ddfcac8f9ce0f
Nishit Ladani <nishitladani2111@gmail.com>
2024-12-09 08:07:58 +0530

    Assignment33

---
+++ src/nishit/Assignment33.java
@@ -0,0 +1,29 @@
+package nishit;
+
+public class Assignment33
+{
+	int getCharCount(String str, char ch) {
+		int count = 0;
+		for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) == ch) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	void printFrequencyOfChars(String str) {
+		for (int i = 0; i < str.length(); i++) {
+			if (str.indexOf(str.charAt(i)) == i) {
+				System.out.println("Frequency of char " + str.charAt(i) + " is - " + getCharCount(str, str.charAt(i)));
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		Assignment32 assignment32 = new Assignment32();
+		String input = "technocredits";
+		System.out.println("Frequency Map for given string " + input + " is as below-");
+		assignment32.printFrequencyOfChars(input);
+	}
+}
