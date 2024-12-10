ffe1a8182a63f64bfdb663b958d1c5d6c24a79eb
Hole <swapnil.hole@t-systems.com>
2024-12-10 08:57:02 +0530

    Swapnil_Assignment_45

--- src/swapnil/Assignment45.java
+++ src/swapnil/Assignment45.java
@@ -3,18 +3,18 @@ package swapnil;
 public class Assignment45 {
 	
 	void pattern(){
-		for(int i=4;i>=1;i--) {
-			for(int j=1;j<=i;j++) {
-				if(j%2!=0){
-					System.out.print(" # ");
+		for(int i=1;i<=2;i++) {
+			for(int j=1;j<=2;j++) {
+				if(j%2!=0) {
+					System.out.println("#*#*#");
 				}
-					else {
-						System.out.print(" * ");
-					}
+				else {
+					System.out.println("#*#");
 				}
-			System.out.println();
 			}
 		}
+	}
+		
 	
 
 	public static void main(String[] args) {
@@ -24,3 +24,16 @@ public class Assignment45 {
 	}
 
 }
+
+//	for(int i=3;i>=2;i--) {
+//		for(int j=1;j<=i;j++) {
+//			if(j%2!=0){
+//				System.out.println(" #*#*# ");
+//			}
+//				else {
+//					System.out.println(" #*# ");
+//				}
+//			}
+//		
+//		}
+//	}
