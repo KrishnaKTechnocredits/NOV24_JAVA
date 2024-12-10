fe2c174a86c7e0c33352f05114d6daa66ec45f38
Roshan Mokati <backuproshan21@gmail.com>
2024-12-10 09:08:14 +0530

    new-roshan_assignment_18_19_20_21

--- src/roshan/ExcludeSpace.java
+++ src/roshan/ExcludeSpace.java
@@ -1,23 +1,28 @@
 package roshan;
 
 public class ExcludeSpace {
-	static int a=0;
-	static int b=0;
-	
-	static void getWithoutSapce (String word){
-		for(int index=0 ;index<word.length();index++) {
-			if( word.charAt(index)==' ') {
-				 a++;
-			}
-			else {
-			 b++;
+	static int a = 0;
+	static int b = 0;
+
+	static void getWithoutSapce(String word) {
+		for (int index = 0; index < word.length(); index++) {
+			if (word.charAt(index) == ' ') {
+				a++;
+			} else {
+				b++;
 			}
 		}
-		}
+	}
+
 	public static void main(String[] args) {
-	 ExcludeSpace.getWithoutSapce("my name is roshan Mokati");
-	 System.out.println("total space count is "+ a + " "+"total alphabet are "+ b);
-		
+		ExcludeSpace.getWithoutSapce("my name is roshan Mokati");
+		System.out.println("total space count is " + a + " " + "total alphabet are " + b);
+
 	}
 
 }
+
+/*
+ * Find the length of the given string excluding space. input : prem lata gupta
+ * output : length without space is 13 and space characters are 2.
+ */
--- src/roshan/Revstringorder.java
+++ src/roshan/Revstringorder.java
@@ -1,15 +1,21 @@
 package roshan;
 
 public class Revstringorder {
-	
-	static void getReverse (String str){
-		for	(int index= str.length()-1 ;index>=0;index--) {
-			System.out.print(str.charAt(index));}
+
+	static void getReverse(String str) {
+		for (int index = str.length() - 1; index >= 0; index--) {
+			System.out.print(str.charAt(index));
+		}
 	}
-	
-	public static void main(String[]args) {
+
+	public static void main(String[] args) {
 		getReverse("Roshan");
 
 	}
-	
+
 }
+
+/*
+ * Assignment - 19 : 24rd Nov'2024 [20 mins] Print all the characters of given
+ * string in reverse order. input : te ch3no output : o n 3 H c e t
+ */
