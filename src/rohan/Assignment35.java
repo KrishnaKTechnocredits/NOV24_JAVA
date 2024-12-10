96e5b8ba65f68a4aa5a42f367c455b83ef8c5ab6
RohanKhochare <RDKhochare@outlook.com>
2024-12-10 07:40:49 +0530

    code modified

--- src/rohan/Assignment35.java
+++ src/rohan/Assignment35.java
@@ -4,39 +4,40 @@ public class Assignment35 {
 
 	public static void main(String[] args) {
 		Assignment35 assignment = new Assignment35();
-		String str =assignment.Frequency("AAB2cBB2BAd2ed3dd");
+		String str = assignment.Frequency("AAB2cBB2BAd2ed3dd");
 		System.out.println(str);
 	}
-	
-	int Count(String str,char ch) {
-		int count=0;
-		for(int i=0;i<str.length();i++) {
-			if(str.charAt(i)==ch) {
+
+	int Count(String str, char ch) {
+		int count = 0;
+		for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) == ch) {
 				count++;
 			}
 		}
 		return count;
 	}
+
 	String Frequency(String str) {
-		for(int i=0;i<str.length();i++) {
-			int upper=0;
-			int lower =0;
+		int upper = 0;
+		int lower = 0;
+		for (int i = 0; i < str.length(); i++) {
+
 			char ch = str.charAt(i);
-			if(Character.isUpperCase(ch)) {
+			if (Character.isUpperCase(ch)) {
 				upper++;
 			}
-			if(Character.isLowerCase(ch)) {
+			if (Character.isLowerCase(ch)) {
 				lower++;
 			}
-			if(upper>lower) {
-				return "upper";
-			}else if(lower>upper) {
-				return "lower";
-			}else {
-				return "same";
-			}
 		}
-		return null;
+		if (upper > lower) {
+			return "upper";
+		} else if (lower > upper) {
+			return "lower";
+		} else {
+			return "same";
+		}
 	}
 
 }
