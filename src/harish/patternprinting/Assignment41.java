84d5b196dc9a3d4c3cfdbe2afbbf6f9653bd9cf2
Harish Rahangdale <hsr.29978@gmail.com>
2024-12-10 21:50:35 +0530

    [Harish][Assignment- 36 to 45]

---
+++ src/harish/patternprinting/Assignment41.java
@@ -0,0 +1,29 @@
+package harish.patternprinting;
+
+/**
+ * Assignment - 41 : 8th Dec
+ * 
+ * Write a logic to print below pattern.
+ */
+// 1 1 1 1
+// 2 2 2 2
+// 3 3 3 3
+// 4 4 4 4
+// 5 5 5 5
+
+public class Assignment41 {
+
+	void printPattern() {
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j <= 4; j++) {
+				System.out.print(i + " ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) {
+		Assignment41 assignment41 = new Assignment41();
+		assignment41.printPattern();
+	}
+}
