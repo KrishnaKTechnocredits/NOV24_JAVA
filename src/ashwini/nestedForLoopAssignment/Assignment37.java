7b263f7c5355889fa717560f0593e74c6c48dfdb
Ashwini Bhuskat <ashwini.bhuskat@gmail.com>
2024-12-10 18:26:48 +0530

    Ashwini Assignment 36-->Print table

---
+++ src/ashwini/nestedForLoopAssignment/Assignment37.java
@@ -0,0 +1,13 @@
+package ashwini.nestedForLoopAssignment;
+
+public class Assignment37 {
+void getFreqNestedForLoop(){
+	for  
+	
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
