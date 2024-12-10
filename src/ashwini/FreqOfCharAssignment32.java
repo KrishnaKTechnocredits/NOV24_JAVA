0a8c6537ce334fbbb0465181b53e86e3ffb0156d
Ashwini Bhuskat <ashwini.bhuskat@gmail.com>
2024-12-10 13:23:19 +0530

    Ashwini Assignment 32-Frequency of character in given string

--- src/ashwini/FreqOfCharAssignment32.java
+++ src/ashwini/FreqOfCharAssignment32.java
@@ -1,18 +1,37 @@
 package ashwini;
 
+//Assignment32-->Find count of each character in the given string :input -aakansha
+
 public class FreqOfCharAssignment32 {
 
-	void freqOfChar(String str) {
+	int freqOfChar(String str,char letter) 
+	{
+		int count=0;
 		for (int i = 0; i < str.length(); i++) {
 			char ch = str.charAt(i);
-			int index = str.indexOf(ch);
-			System.out.println(ch + "-->" + i + "-->" + index);
+			if(letter==ch) {
+				count++;
+			}
 		}
+		return count;
 	}
-
-	public static void main(String[] args) {
+		
+		void printFreqOfChar(String input) {
+			for(int j=0;j<input.length();j++) {
+				char ch1=input.charAt(j);
+				FreqOfCharAssignment32 assn32=new FreqOfCharAssignment32();
+				int ans= assn32.freqOfChar(input, ch1);
+				int index = input.indexOf(ch1);
+				if(j==index) {
+				System.out.println("Letter " +ch1 + " frequency is " + ans);
+				}
+		}
+		}	
+			
+	     public static void main(String[] args) {
 		FreqOfCharAssignment32 assn32 = new FreqOfCharAssignment32();
-		assn32.freqOfChar("aakanksha");
+		assn32.printFreqOfChar("aakanksha");
+		
 	}
 
 }
