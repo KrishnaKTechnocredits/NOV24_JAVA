5bd1b197c6572c02cf7d158aeab7abded7ddc4c5
Kavita Jadhav <kavyajadhav1905@gmail.com>
2024-12-10 09:39:05 +0530

    Kavita_Assignment_33to45

---
+++ src/kavita/Assignment39.java
@@ -0,0 +1,24 @@
+package kavita;
+/*Assignment - 39 : 8th Dec[15 mins]
+
+write a logic to print below pattern.
+
+1
+1 2
+1 2 3
+1 2 3 4
+Time: 6min*/
+public class Assignment39 {
+	 void printNumbers() {
+		 for(int i=1;i<=4;i++) {
+			 for(int j=1;j<=j;j++) {
+				 System.out.print(j+" ");
+			 }
+		 }
+	 }
+	 
+	 public static void main(String[] args) {
+		 Assignment39 assignment39=new Assignment39();
+		 assignment39.printNumbers();
+	}
+}
