7f7ffaea7b9d634fbf3883ce17a3805877e01a09
Prem Lata Gupta <premlata.gupta5@gmail.com>
2024-12-12 23:53:03 +0530

    corrected logic

--- src/prem/Assignment45.java
+++ src/prem/Assignment45.java
@@ -1,22 +1,33 @@
 package prem;
 
 public class Assignment45 {
-	
+
 	void pattern() {
-		String pattern1 ="#*#";
-		String pattern2 ="#*#*#";		
-		for (int i =1; i<=4;i++){
-			if(i%2==0) {
-				System.out.println(pattern1);
-			}else {
-				System.out.println(pattern2);
+		String star = "*";
+		String hash = "#";
+
+		for (int i = 1; i <= 4; i++) {
+			if (i % 2 == 0) {
+				for (int j = 1; j <= 3; j++) {
+					if (j % 2 != 0) {
+						System.out.print(hash);
+					} else
+						System.out.print(star);
+				}
+			} else {
+				for (int j = 1; j <= 5; j++) {
+					if (j % 2 != 0) {
+						System.out.print(hash);
+					} else
+						System.out.print(star);
+				}
 			}
-			
+			System.out.println();
 		}
 	}
-	
+
 	public static void main(String[] args) {
-		Assignment45 assign45 =new Assignment45();
+		Assignment45 assign45 = new Assignment45();
 		assign45.pattern();
 	}
 }
