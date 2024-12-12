d20bdd41dc70a6a8dbdb36f1235d248e5f53ba9a
Prem Lata Gupta <premlata.gupta5@gmail.com>
2024-12-12 23:09:02 +0530

    corrected if logic

--- src/prem/Assignment22.java
+++ src/prem/Assignment22.java
@@ -2,20 +2,20 @@ package prem;
 
 public class Assignment22 {
 	static int count;
-	
+
 	int getTotalNumDigit(String str) {
-		for(int i=0;i<=str.length()-1;i++){
-       	boolean flag =Character.isDigit(str.charAt(i));
-          if (flag == true) {
-			count++;			
-		    }
+		for (int i = 0; i <= str.length() - 1; i++) {
+			boolean flag = Character.isDigit(str.charAt(i));
+			if (flag == true) {
+				count++;
+			}
 		}
-		return count;	
+		return count;
 	}
-	
+
 	public static void main(String[] args) {
-		Assignment22 assign22 =new Assignment22();
+		Assignment22 assign22 = new Assignment22();
 		int digit = assign22.getTotalNumDigit("aa1kan33ks4h5a");
-		System.out.println("Total "+ digit +" digits are there in given input.");			
+		System.out.println("Total " + digit + " digits are there in given input.");
 	}
 }
