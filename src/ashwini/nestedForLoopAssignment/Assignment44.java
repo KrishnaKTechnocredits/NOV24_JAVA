341719db611a470805c059cf6145e870b3dcadb6
Ashwini Bhuskat <ashwini.bhuskat@gmail.com>
2024-12-13 13:52:48 +0530

    Ashwini Assignment44-->pattern print

---
+++ src/ashwini/nestedForLoopAssignment/Assignment44.java
@@ -0,0 +1,28 @@
+package ashwini.nestedForLoopAssignment;
+/*write a login to print below pattern
+*
+* #
+* # *
+* # * #
+* # * # *  */
+
+public class Assignment44 {
+	void printPattern() {
+		for(int i=1;i<=5;i++) {
+			for (int j=1;j<=i;j++) {
+				if(j%2==0) {
+					System.out.print("# ");
+				}else {
+					System.out.print("* ");
+				}
+				
+			}
+			System.out.println("\n");
+		}
+	}
+	public static void main(String[] args) {
+		Assignment44 assn44=new Assignment44();
+		assn44.printPattern();
+	}
+
+}
