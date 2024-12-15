107c79798f37fc8614a007d356e520d0abcd016b
Prashant2020 <pawar7smailbox2.com>
2024-12-15 22:58:00 +0530

    Prashant_Assignment42

---
+++ src/prashant/Assignment42.java
@@ -0,0 +1,31 @@
+/*Assignment - 42 : 8th Dec[10 mins]
+
+write a logic to print below pattern.
+
+1
+2 2
+3 3 3
+4 4 4 4*/
+
+package prashant;
+
+public class Assignment42 {
+	
+	void printPattern() {
+		for(int i=1;i<=4;i++) {
+			for (int j=1;j<=i;j++) {
+				System.out.print(i+" ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) {
+		
+		Assignment42 assignment42= new Assignment42();
+		assignment42.printPattern();
+		
+
+	}
+
+}
