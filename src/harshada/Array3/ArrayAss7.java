9f9374b77c49e3f2f1dccca203f3b45c3b4dbafe
Harshada JagtapW <jagtap.harshada.11@gmail.com>
2024-12-23 01:58:09 +0530

    Harshada_assignment54

---
+++ src/harshada/Array3/ArrayAss7.java
@@ -0,0 +1,28 @@
+//Assignment - 54 : 18th Dec'2024 [20 mins]
+//return the array having elements in reverse order of original array.
+//
+//int[] getReverseArray(int[] arr){
+//	
+//} 
+
+package harshada.Array3;
+
+import java.util.Arrays;
+
+public class ArrayAss7 {
+	
+	int[] getReverseArray(int[] arr) {
+		int[] output= new int[arr.length];
+		for(int j=arr.length-1,i=0;j>=0 ;j--,i++) {
+			output[i]=arr[j];
+		}
+		return output;		
+	}
+	
+	public static void main(String[] args) {
+		ArrayAss7 arrayAss7=new ArrayAss7();
+		int[] input={10,55,22,77,88};
+		int[] output=arrayAss7.getReverseArray(input);
+		System.out.println(Arrays.toString(output));
+	}
+}
