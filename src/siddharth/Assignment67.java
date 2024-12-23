a1076c6325924c7b32011a48004a079f1dd43156
Siddharth Kale <siddharth.kale@ubisoft.com>
2024-12-23 23:05:30 +0530

    Siddharth_Assignment67_68

---
+++ src/siddharth/Assignment67.java
@@ -0,0 +1,36 @@
+package siddharth;
+/*
+Assignment - 67 : 23rd Dec (IMP) [15 mins]
+Rearrange the given String, uppercase followed by digits followed by lowercase.
+
+input : Te1ch6NOCre3dits
+output : TNOC163echredits
+ */
+public class Assignment67 {
+	
+	void RearrangeString(String word) {
+		String output1="";
+		String output2="";
+		String output3="";
+		for(int i=0;i<word.length();i++) {
+			char ch=word.charAt(i);
+				if(Character.isUpperCase(ch)) {
+					output1=output1+ch;
+				}
+				else if (Character.isDigit(ch)) {
+					output2=output2+ch;
+				}	
+				else if(Character.isLowerCase(ch)) {
+					output3=output3+ch;
+				}
+		}
+		String output=output1+output2+output3;
+		System.out.println("Output : "+output);
+	}
+	
+	public static void main(String[] args) {
+		Assignment67 assignment67 = new Assignment67();
+		String input="Te1ch6NOCre3dits";
+		assignment67.RearrangeString(input);
+	}
+}
---
+++ src/siddharth/Assignment68.java
@@ -0,0 +1,37 @@
+package siddharth;
+/*
+Assignment - 68 : 23rd Dec (IMP) [15 mins]
+Rearrange the given String, uppercase followed by sum of all the digits followed by lowercase.
+
+input : Te1ch6NOCre4dits
+output : TNOC11echredits 
+ */
+public class Assignment68 {
+	
+	void RearrangeString(String word) {
+		String output1="";
+		int output2=0;
+		String output3="";
+		for(int i=0;i<word.length();i++) {
+			char ch=word.charAt(i);
+				if(Character.isUpperCase(ch)) {
+					output1=output1+ch;
+				}
+				else if (Character.isDigit(ch)) {
+					output2=output2+Character.getNumericValue(ch);
+				}	
+				else if(Character.isLowerCase(ch)) {
+					output3=output3+ch;
+				}
+		}
+		String output=output1+output2+output3;
+		System.out.println("Output : "+output);
+	}
+	
+	public static void main(String[] args) {
+		Assignment68 assignment68 = new Assignment68();
+		String input="Te1ch6NOCre4dits";
+		assignment68.RearrangeString(input);
+	}
+}
+
