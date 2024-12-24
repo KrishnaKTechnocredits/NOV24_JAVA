2a1cc9b0b1b9dac201be9c9d2f2a8540df6a6e81
PagarAjay <pagarajay138@gmail.com>
2024-12-24 15:44:48 +0530

    Changes Suggested by Tejas

--- src/ajay_ArrayAssignments/Assignment46.java
+++ src/ajay_ArrayAssignments/Assignment46.java
@@ -4,18 +4,25 @@ public class Assignment46 {
 
 	void getMiddileOfString(String[] input) {
 		System.out.println("Middle Character of each String in given array is : ");
-		for (int i = 0; i < input.length; i++) {
+		
+		for (int i = 0; i < input.length; i++) {//For Even Length String
+			
 			int index = input[i].length() / 2;
-
-			char ch = input[i].charAt(index);
-			System.out.println(input[i] + "-> " + ch);
+			
+			if(input[i].length()%2==0) {
+	
+			System.out.println(input[i] + "-> "+input[i].charAt(index-1)+ input[i].charAt(index));
+			}
+			else {//For Odd Length String
+				System.out.println(input[i] + "-> "+ input[i].charAt(index));
+			}
 		}
 
 	}
 
 	public static void main(String[] args) {
 		Assignment46 assignment46 = new Assignment46();
-		String[] input = { "Rohan", "Premlata", "Anuja", "Fazrana" };
+		String[] input = { "Rohan", "Premlata", "Anuja", "Fazrana","Ajay","Maulik" };
 		assignment46.getMiddileOfString(input);
 	}
 
