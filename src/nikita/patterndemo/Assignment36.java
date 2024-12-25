848d3be105ed917aab01078af3261d1cc99dedba
Nikitaw-1011 <nikitawani29@gmail.com>
2024-12-25 12:38:11 +0530

    print table for 1 to 5 numbers.

---
+++ src/nikita/patterndemo/Assignment36.java
@@ -0,0 +1,21 @@
+/* print table for 1 to 5 numbers.*/
+
+package nikita.patterndemo;
+
+public class Assignment36 {
+	void m1() {
+		for(int i = 1; i<=5; i++) {
+			System.out.println("Table for number " + i + ":");
+			for(int j = 1; j<=10; j++) {
+				System.out.println(i + " * " + j + " = " + (i*j));		
+				}
+			System.out.println("------------------------");
+		}
+		System.out.println();
+	}
+	
+	public static void main(String[] args) {
+		Assignment36 a36 = new Assignment36();
+		a36.m1();
+	}
+}
