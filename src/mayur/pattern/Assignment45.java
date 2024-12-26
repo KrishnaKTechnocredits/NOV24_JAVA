4ce82f177142f17f27fbd1e079b1e6240c00893b
unknown <wanimayur91@gmail.com>
2024-12-26 11:36:49 +0530

    correction done in Assignment45

--- src/mayur/pattern/Assignment45.java
+++ src/mayur/pattern/Assignment45.java
@@ -12,11 +12,21 @@ package mayur.pattern;
 public class Assignment45 {
 	public static void main(String[] args) {
 		for(int i=1;i<=4;i++) {
+			int k = 0;
 			if (i%2!=0) {
-				System.out.println("#*#*#");
+				k=5;
 			}else {
-				System.out.println("#*#");
+				k=3;
 			}
+			for (int j=1;j<=k;j++) {
+				if (j%2 != 0) {
+					System.out.print("#");
+				}
+				else {
+					System.out.print("*");
+				}
+			}
+			System.out.println();
 		}
 	}
 }
