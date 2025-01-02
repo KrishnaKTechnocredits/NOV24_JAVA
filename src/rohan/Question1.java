e7ca9e05ed7ebfe58722cb8f04347268b2b8f5a6
RohanKhochare <RDKhochare@outlook.com>
2025-01-02 10:10:26 +0530

    Rohan Modified Test

--- src/rohan/Question1.java
+++ src/rohan/Question1.java
@@ -19,21 +19,22 @@ public class Question1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Question1 ques = new Question1();
-		int[] input = {3, 5, 7, -1, 9};
+		int[] input = { 3, 5, 7, -1, 9 };
 		int[] Output = ques.SumArray(input);
 		System.out.println(Arrays.toString(Output));
 	}
+
 	int[] SumArray(int[] input) {
-		
+
 		int[] output = new int[input.length];
-		for(int i=0;i<input.length;i++){
+		for (int i = 0; i < input.length; i++) {
 			int sum = 0;
-			for(int j=0;j<input.length;j++) {
-			if(input[i]!=input[j] ) {
-				sum += input[j];
+			for (int j = 0; j < input.length; j++) {
+				if (input[i] != input[j]) {
+					sum += input[j];
+				}
 			}
-			}
-			output[i]=sum;
+			output[i] = sum;
 		}
 		return output;
 	}
--- src/rohan/Question2.java
+++ src/rohan/Question2.java
@@ -17,26 +17,37 @@ public class Question2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Question2 ques = new Question2();
-		String[] input = {"apple", "sky", "orange", "try", "eagle"};
+		String[] input = { "apple", "sky", "orange", "try", "eagle" };
 		String[] Output = ques.RevArray(input);
 		System.out.println(Arrays.toString(Output));
 	}
-	
+
 	String ReverseString(String Str) {
 		String word = "";
-		for(int i=Str.length()-1; i>=0 ;i--) {
+		for (int i = Str.length() - 1; i >= 0; i--) {
 			word += Str.charAt(i);
 		}
 		return word;
 	}
 
+	int CountVowel(String Str) {
+		int count = 0;
+		for (int i = 0; i < Str.length(); i++) {
+			char ch = Str.charAt(i);
+			if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+				count++;
+			}
+		}
+		return count;
+	}
+
 	String[] RevArray(String[] input) {
 		String[] Reverse = new String[input.length];
-		for(int i=0;i< input.length;i++) {
-			if(i%2==0) {
+		for (int i = 0; i < input.length; i++) {
+			if (CountVowel(input[i]) > 1) {
 				Reverse[i] = ReverseString(input[i]);
-			}else {
-			Reverse[i]=input[i];
+			} else {
+				Reverse[i] = input[i];
 			}
 		}
 		return Reverse;
