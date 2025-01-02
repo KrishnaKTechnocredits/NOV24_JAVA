729c4ed7a172f70304b25e71564b93e22ca7768e
unknown <wanimayur91@gmail.com>
2025-01-02 10:51:20 +0530

    mayur_challenge2_correction

--- src/mayur/Challenge2.java
+++ src/mayur/Challenge2.java
@@ -16,10 +16,12 @@ import java.util.Arrays;
 public class Challenge2 {
 	boolean checkVowel(String str) {
 		boolean flag = false;
+		int count = 0;
 		str=str.toLowerCase();
 		for (int i = 0; i<str.length();i++) {
 			if (str.charAt(i)=='a' || str.charAt(i)=='e' || str.charAt(i)=='i' || str.charAt(i)=='o' || str.charAt(i)=='u') {
-				if (str.charAt(i)=='a' || str.charAt(i)=='e' || str.charAt(i)=='i' || str.charAt(i)=='o' || str.charAt(i)=='u') {
+				count++;
+				if (count > 1 ) {
 					flag = true;
 				}
 			}
