93a015f1168a7705db96709176465467af6954df
Harshada Jagtap <jagtap.harshada.11@gmail.com>
2025-01-02 09:51:26 +0530

    Modified formatting in Programming Test

--- src/harshada/Test1.java
+++ src/harshada/Test1.java
@@ -5,29 +5,28 @@ Example:
 Input: int[] input = {3, 5, 7, 1, 9};
 Output: int[] output = {22, 20, 18, 24, 16};*/
 
-
 package harshada;
 
 import java.util.Arrays;
 
 public class Test1 {
-	
-	int[] sumOfOtherElementArray(int[] arr) {	
-		int[]output=new int[arr.length];	
-		for(int i=0;i<arr.length;i++) {
-			for(int j=0;j<arr.length;j++) {
-				if(i!=j) {
-					output[i]=output[i]+arr[j];
+
+	int[] sumOfOtherElementArray(int[] arr) {
+		int[] output = new int[arr.length];
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr.length; j++) {
+				if (i != j) {
+					output[i] = output[i] + arr[j];
 				}
 			}
 		}
 		return output;
 	}
-	
+
 	public static void main(String[] args) {
-		Test1 test1=new Test1();
-		int[] input = {3, 5, 7, 1, 9};
-		int[] output=test1.sumOfOtherElementArray(input);
+		Test1 test1 = new Test1();
+		int[] input = { 3, 5, 7, 1, 9 };
+		int[] output = test1.sumOfOtherElementArray(input);
 		System.out.println(Arrays.toString(output));
 	}
 
--- src/harshada/Test2.java
+++ src/harshada/Test2.java
@@ -8,27 +8,26 @@ String[] input = {"apple", "sky", "orange", "try", "eagle"};
 Output:
 String[] output = {"elppa", "sky", "egnaro", "try", "elgae"};*/
 
-
 package harshada;
 
 import java.util.Arrays;
 
 public class Test2 {
-	
+
 	public static void main(String[] args) {
-		String[] input = {"apple", "sky", "orange", "try", "eagle"};
-		Test2 test2=new Test2();
+		String[] input = { "apple", "sky", "orange", "try", "eagle" };
+		Test2 test2 = new Test2();
 		test2.printReverseStringArrayWithVowels(input);
 	}
-	
+
 	void printReverseStringArrayWithVowels(String[] input) {
-		String[] output=new String[input.length];
-		for(int i=0;i<input.length;i++) {
-			output[i]=countVowelsInString(input[i]);//calling method to check vowels in given string
+		String[] output = new String[input.length];
+		for (int i = 0; i < input.length; i++) {
+			output[i] = countVowelsInString(input[i]);// calling method to check vowels in given string
 		}
 		System.out.println(Arrays.toString(output));
 	}
-	
+
 	String countVowelsInString(String input) {
 		int vowelcount = 0;
 		String vowelString = "aeiouAEIOU";
@@ -38,21 +37,21 @@ public class Test2 {
 					vowelcount++;
 				}
 			}
-		}//System.out.println(vowelcount);
-		String output="";
-		if(vowelcount>0) {
-			 output=revString(input);//calling method to reverse string if it has vowels
-			 return output;//return string with vowels 
+		} // System.out.println(vowelcount);
+		String output = "";
+		if (vowelcount > 0) {
+			output = revString(input);// calling method to reverse string if it has vowels
+			return output;// return string with vowels
 		}
-		return input;//return string without vowels
+		return input;// return string without vowels
 	}
 
 	String revString(String input) {
-		String output="";
-		for(int i=input.length()-1;i>=0;i--) {
-			output+=input.charAt(i);
+		String output = "";
+		for (int i = input.length() - 1; i >= 0; i--) {
+			output += input.charAt(i);
 		}
-		//System.out.println(output);
+		// System.out.println(output);
 		return output;
 	}
 }
