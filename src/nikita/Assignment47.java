664859387da9264eaa0b2f36d3859e901abddf36
Nikitaw-1011 <nikitawani29@gmail.com>
2025-01-05 12:15:49 +0530

    Print only names having length more than 6 characters from given String array.

---
+++ src/nikita/Assignment47.java
@@ -0,0 +1,27 @@
+/* Assignment - 47 : 15th Dec'2024 [12 mins]
+Print only names having length more than 6 characters from given String array.
+String[] names = {"Rohan", "Premlata", "Anuja", "Fazrana"};
+
+output : 
+Premlata
+Fazrana
+*/
+
+package nikita;
+
+public class Assignment47 {
+	
+	void m1(String[] names) {
+		for(int i = 0; i<names.length; i++) {
+			if (names[i].length() > 6) {
+				System.out.println(names[i]);
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		String[] input = {"Rohan", "Premlata", "Anuja", "Fazrana"};
+		Assignment47 a47 = new Assignment47();
+		a47.m1(input);
+	}
+}
