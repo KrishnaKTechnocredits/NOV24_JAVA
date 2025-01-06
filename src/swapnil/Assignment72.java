e1adf0d331751a3092c53c2e49f20c5577724e45
Hole <swapnil.hole@t-systems.com>
2025-01-06 23:47:11 +0530

    Swapnil_Assignment_72_73

---
+++ src/swapnil/Assignment72.java
@@ -0,0 +1,29 @@
+package swapnil;
+
+import java.util.Arrays;
+
+public class Assignment72 {
+	
+	void shiftZeroToRight(int[] arr) {
+		int j =0;
+		int[] s = new int[arr.length];
+		
+		for(int i=0;i<arr.length-1;i++) {
+			
+			if(arr[i]!=0) {
+				s[j]=arr[i];
+				j++;
+			}
+		}
+		System.out.println(Arrays.toString(s));;
+	}
+
+	public static void main(String[] args) {
+		int[] arr = {5,0,22,44,0,0,55,0,12,19};
+		Assignment72 a = new Assignment72();
+		a.shiftZeroToRight(arr);
+		
+
+	}
+
+}
---
+++ src/swapnil/Assignment73.java
@@ -0,0 +1,30 @@
+package swapnil;
+
+import java.util.Arrays;
+
+public class Assignment73 {
+	
+	void shiftZeroToRight(int[] arr) {
+		int j =4;
+		int[] s = new int[arr.length];
+		
+		for(int i=0;i<arr.length-1;i++) {
+			
+			if(arr[i]!=0) {
+				s[j]=arr[i];
+				j++;
+			}
+		}
+		System.out.println(Arrays.toString(s));;
+	}
+
+	public static void main(String[] args) {
+		int[] arr = {5,0,22,44,0,0,55,0,12,19};
+		Assignment73 a = new Assignment73();
+		a.shiftZeroToRight(arr);
+		
+
+	}
+
+}
+
