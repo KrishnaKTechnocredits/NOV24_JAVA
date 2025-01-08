746cd1d74ad8dd1b631432bb102a0e5409309abe
komal-1993 <komalkhopade5@gmail.com>
2025-01-08 09:44:28 +0530

    test program

---
+++ src/komal/TestProgram/Assignment74.java
@@ -0,0 +1,34 @@
+package komal.TestProgram;
+
+//Assignment - 74 : 8th Jan
+//Rotate all the numbers of an array to the right by one position. 
+//input : {10,20,30,40,50}
+//output : {50,10,20,30,40};
+//     {40,50,10,20,30}
+
+import java.util.Arrays;
+
+public class Assignment74 {
+	static int count = 0;
+
+	int[] getRotateArray(int[] input) {
+		count++;
+		int output[] = new int[input.length];
+		int last = input[input.length - 1];
+		for (int i = input.length - 1; i > 0; i--) {
+			output[i] = input[i - 1];
+		}
+		output[0] = last;
+		return output;
+	}
+
+	public static void main(String[] args) {
+		Assignment74 a74 = new Assignment74();
+		int[] input = { 10, 20, 30, 40, 50 };
+		int[] output = a74.getRotateArray(input);
+		if (Assignment74.count == 1) {
+			output = a74.getRotateArray(output);
+		}
+		System.out.println("output Array is : " + Arrays.toString(output));
+	}
+}
