d9eef71daec3cb23f869357d14b04144871f24e8
Pramod <pramodmane1122@gmail.com>
2025-01-08 21:42:38 +0530

    Pramod_Assignment73_CodeLoginUpdated

--- src/pramod/arraydemo/Assignment73.java
+++ src/pramod/arraydemo/Assignment73.java
@@ -14,15 +14,10 @@ class Assignment73{
 	void printAllZeroAtStartOfArray(int[] arr){
 		int[] output = new int[arr.length];
 		int arrLength = arr.length-1;
-		int countOfI = 0;
-		for (int i=0; i< arr.length; i++){
+		for (int i= arr.length-1; i>=0; i--){
 			if(arr[i]!=0){
 				output[arrLength]=arr[i];
 				arrLength--;
-			}
-			else {
-				arr[countOfI]=arr[i];
-				countOfI++;
 			}
 		}
 		System.out.println("All zero at the start of Array : "+ Arrays.toString(output));
