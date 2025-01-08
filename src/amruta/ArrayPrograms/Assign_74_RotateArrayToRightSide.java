e156dc023d35df7dd0698a9dae867ad694aae68a
Amruta <amruta15kharat@gmail.com>
2025-01-08 09:24:45 +0530

    Amruta_Assign_74_changes

--- src/amruta/ArrayPrograms/Assign_74_RotateArrayToRightSide.java
+++ src/amruta/ArrayPrograms/Assign_74_RotateArrayToRightSide.java
@@ -23,12 +23,22 @@ public class Assign_74_RotateArrayToRightSide {
 
 	public static void main(String[] args) {
 		int[] arr = { 10, 20, 30, 40, 50 };
+		System.out.println("Original Array : " + Arrays.toString(arr));
 		Assign_74_RotateArrayToRightSide a74 = new Assign_74_RotateArrayToRightSide();
+		System.out.println("Rotating Array : ");
 		int[] op1 = a74.shifArrayElementsToRight(arr);
 		System.out.println(Arrays.toString(op1));
+
 		int[] op2 = a74.shifArrayElementsToRight(op1);
 		System.out.println(Arrays.toString(op2));
+
 		int[] op3 = a74.shifArrayElementsToRight(op2);
 		System.out.println(Arrays.toString(op3));
+
+		int[] op4 = a74.shifArrayElementsToRight(op3);
+		System.out.println(Arrays.toString(op4));
+
+		int[] op5 = a74.shifArrayElementsToRight(op4);
+		System.out.println(Arrays.toString(op5));
 	}
 }
