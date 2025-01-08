14c592682af24ba249292845106e987a6710b215
Hole <swapnil.hole@t-systems.com>
2025-01-08 09:34:02 +0530

    Swapnil_Assignment_74

--- src/swapnil/Assignment74.java
+++ src/swapnil/Assignment74.java
@@ -6,13 +6,16 @@ public class Assignment74 {
 	
 	void ShiftToRight(int[] arr) {
 		int[] Array = new int[arr.length];
-		
+		int[]Array1 = new int[Array.length];		
 		for(int i=0;i<arr.length-1;i++) {
 			
 			Array[i+1] = arr[i];
 			Array[0] = arr[arr.length-1];
+			Array1[0]=Array[Array.length-1];
+			Array1[i+1]=Array[i];
 		}
 		System.out.println(Arrays.toString(Array));
+		System.out.println(Arrays.toString(Array1));
 	}
 
 	public static void main(String[] args) {
