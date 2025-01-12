08d510606e4097cf1e0f0da440fb3237994b79d6
PARWEEN Fazrana <D4J@dsone.3ds.com>
2025-01-12 13:42:25 +0530

    Fazrana_Assignment77

---
+++ src/fazrana/arrayList/FAssignment77.java
@@ -0,0 +1,56 @@
+//Assignment - 77 : 11th Jan
+//Remove all duplicate occurances of given name from arrayList except first occurance. 
+//
+//input : 
+//ArrayList<String> al = new ArrayList<String>(100);
+//		al.add("Anuja");
+//		al.add("Fazrana");
+//		al.add("Nitesh");
+//		al.add("Fazrana");
+//		al.add("Krishna");
+//		al.add("Nitesh");
+//		al.add("Fazrana");
+//		al.add("PremLata");
+//		al.add("Nitesh");
+//		al.add("Nitesh");
+//		al.add("Nitesh");
+//		al.add("Aashvi");
+//
+//output : [Anuja, Fazrana, Nitesh, Fazrana, Krishna, Fazrana, PremLata, Aashvi]
+
+package fazrana.arrayList;
+
+import java.util.*;
+
+public class FAssignment77 {
+	List<String> removeName(String name, List<String> arrayList) {
+		for (int i = arrayList.size() - 1; i >= 0; i--) {
+			String name1 = arrayList.get(i);
+			if (name1 == name) {
+				if (arrayList.lastIndexOf(name) != arrayList.indexOf(name)) {
+					arrayList.remove(arrayList.lastIndexOf(name));
+				}
+			}
+		}
+		return arrayList;
+	}
+
+	public static void main(String[] args) {
+		FAssignment77 fassignment77 = new FAssignment77();
+		ArrayList<String> arrayList = new ArrayList<String>();
+		arrayList.add("Anuja");
+		arrayList.add("Fazrana");
+		arrayList.add("Nitesh");
+		arrayList.add("Fazrana");
+		arrayList.add("Krishna");
+		arrayList.add("Nitesh");
+		arrayList.add("Fazrana");
+		arrayList.add("PremLata");
+		arrayList.add("Nitesh");
+		arrayList.add("Nitesh");
+		arrayList.add("Nitesh");
+		arrayList.add("Aashvi");
+		System.out.println("Input: " + arrayList);
+		System.out.println("Output: " + fassignment77.removeName("Nitesh", arrayList));
+	}
+}
