f6966a9501ff863c62f4f3b37af2f6aa8900fc42
Hole <swapnil.hole@t-systems.com>
2025-01-12 19:06:39 +0530

    changes incorporated

--- src/swapnil/Assignment74.java
+++ src/swapnil/Assignment74.java
@@ -3,25 +3,29 @@ package swapnil;
 import java.util.Arrays;
 
 public class Assignment74 {
-	
-	void ShiftToRight(int[] arr) {
+
+	int[] ShiftToRight(int[] arr) {
 		int[] Array = new int[arr.length];
-		int[]Array1 = new int[Array.length];		
-		for(int i=0;i<arr.length-1;i++) {
+		Array[0] = arr[arr.length-1];
 			
+		for(int i=0;i<arr.length-1;i++) {
 			Array[i+1] = arr[i];
-			Array[0] = arr[arr.length-1];
-			Array1[0]=Array[Array.length-1];
-			Array1[i+1]=Array[i];
+	
 		}
-		System.out.println(Arrays.toString(Array));
-		System.out.println(Arrays.toString(Array1));
+		return Array;
+		
 	}
 
 	public static void main(String[] args) {
 		int[] arr = {10,20,30,40,50};
 		Assignment74 a = new Assignment74();
-		a.ShiftToRight(arr);
+		System.out.println(Arrays.toString(arr));
+		int[] A=a.ShiftToRight(arr);
+		System.out.println(Arrays.toString(A));
+		
+		int[] B=a.ShiftToRight(A);
+		System.out.println(Arrays.toString(B));
+		
 	}
 
 }
