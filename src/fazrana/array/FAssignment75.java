3d0354bae550ddfc2305c2f9e80e6cac18fa8e54
PARWEEN Fazrana <D4J@dsone.3ds.com>
2025-01-12 13:30:10 +0530

    Fazrana_Assignment75

---
+++ src/fazrana/array/FAssignment75.java
@@ -0,0 +1,47 @@
+//Assignment - 75 : 11th Jan
+//Return the list of all the prime numbers from given array. 
+//input : {10,23,31,40,50}
+//output : {23,31}
+
+package fazrana.array;
+
+import java.util.Arrays;
+
+public class FAssignment75 {
+
+	boolean isNumberPrime(int num) {
+		boolean value = false;
+		for (int i = 2; i < Math.sqrt(num); i++) {
+			if (num % i == 0) {
+				value = false;
+				break;
+			} else {
+				value = true;
+			}
+		}
+		return value;
+	}
+
+	int[] getPrimeArray(int[] array) {
+		int count = 0;
+		for (int i = 0; i < array.length; i++) {
+			if (isNumberPrime(array[i])) {
+				count++;
+			}
+		}
+		int[] outputArray = new int[count];
+		for (int i = 0, j = 0; i < array.length; i++) {
+			if (isNumberPrime(array[i])) {
+				outputArray[j++] = array[i];
+			}
+		}
+		return outputArray;
+	}
+
+	public static void main(String[] args) {
+		int[] input = { 10, 23, 31, 40, 50 };
+		System.out.println("Input Array:" + Arrays.toString(input));
+		FAssignment75 fassignment75 = new FAssignment75();
+		System.out.println("Prime Number array: " + Arrays.toString(fassignment75.getPrimeArray(input)));
+	}
+}
