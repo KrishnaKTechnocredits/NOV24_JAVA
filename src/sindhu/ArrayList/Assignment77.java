5050f0e4f85d84273ecb881158eff18f1f9d6267
sindhu-windows <sindhupatil32@gmail.com>
2025-01-14 19:20:19 +0530

    Assignment76-80

---
+++ src/sindhu/ArrayList/Assignment77.java
@@ -0,0 +1,51 @@
+/*Remove all duplicate occurances of given name from arrayList except first occurance. 
+
+input : 
+ArrayList<String> al = new ArrayList<String>(100);
+		al.add("Anuja");
+		al.add("Fazrana");
+		al.add("Nitesh");
+		al.add("Fazrana");
+		al.add("Krishna");
+		al.add("Nitesh");
+		al.add("Fazrana");
+		al.add("PremLata");
+		al.add("Nitesh");
+		al.add("Nitesh");
+		al.add("Nitesh");
+		al.add("Aashvi");
+
+output : [Anuja, Fazrana, Nitesh, Fazrana, Krishna, Fazrana, PremLata, Aashvi]*/
+
+package sindhu.ArrayList;
+
+import java.util.*;
+
+public class Assignment77 {
+	public static void main(String[] args) {
+		ArrayList<String> al = new ArrayList<String>(100);
+		al.add("Anuja");
+		al.add("Fazrana");
+		al.add("Nitesh");
+		al.add("Fazrana");
+		al.add("Krishna");
+		al.add("Nitesh");
+		al.add("Fazrana");
+		al.add("PremLata");
+		al.add("Nitesh");
+		al.add("Nitesh");
+		al.add("Nitesh");
+		al.add("Aashvi");
+		
+		ArrayList<String> outAl = new ArrayList<String>();
+		
+		for(int i=0; i<al.size(); i++) {
+			String str = al.get(i); 
+			if(al.indexOf(str)!= al.lastIndexOf(str)) {
+				al.remove(al.lastIndexOf(str));
+				i--;
+			}
+		}
+		System.out.println(al);
+	}
+}
