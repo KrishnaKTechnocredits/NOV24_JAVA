1f0cd3343b2b80e96a7b827d492d67091aa1e566
komal-1993 <komalkhopade5@gmail.com>
2025-01-27 15:55:40 +0530

    comparator&comparable assignment

---
+++ src/komal/Comparator/RevenueComparator.java
@@ -0,0 +1,13 @@
+package komal.Comparator;
+
+import java.util.Comparator;
+
+public class RevenueComparator implements Comparator<Builder> {
+
+	@Override
+	public int compare(Builder o1, Builder o2) {
+		// TODO Auto-generated method stub
+		return (o1.revenue - o2.revenue);
+	}
+
+}
