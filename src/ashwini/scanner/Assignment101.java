a19c639c898ddf2002e885fb5d35fd3feb4fe239
Ashwini Bhuskat <ashwini.bhuskat@gmail.com>
2025-02-01 23:34:26 +0530

    Ashwini Assignment 100-Word freq using scanner class

---
+++ src/ashwini/scanner/Assignment101.java
@@ -0,0 +1,10 @@
+package ashwini.scanner;
+/*
+ Assignment - 101 : 1st Feb'2025
+On user defined numbers, find out the second largest number. 
+Hint : Use scanner.
+ */
+
+public class Assignment101 {
+
+}
