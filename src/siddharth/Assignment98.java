78fba0f95210b27a79e028c5302fd1dbab9df869
Siddharth Kale <siddharth.kale@ubisoft.com>
2025-02-04 00:16:32 +0530

    Siddharth_Assignment97to100

--- src/siddharth/Assignment98.java
+++ src/siddharth/Assignment98.java
@@ -4,5 +4,10 @@ Assignment - 98 : 30th Jan'2025
 Create a class called Connection and apply singleton design pattern.
  */
 public class Assignment98 {
-
+	
+	public static void main(String[] args) {
+		System.out.println(Assignment98LazySingletonConnection.getObject());
+		Assignment98LazySingletonConnection con1 = Assignment98LazySingletonConnection.getObject();
+		System.out.println(con1);
+	}
 }
