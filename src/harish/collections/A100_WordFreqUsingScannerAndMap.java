eaf07e53d2442f44b5893d09683aedaa7bd92188
Harish Rahangdale <hsr.29978@gmail.com>
2025-02-09 23:09:42 +0530

    [Harish][Assignment 100]

---
+++ src/harish/collections/A100_WordFreqUsingScannerAndMap.java
@@ -0,0 +1,33 @@
+package harish.collections;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ * Assignment - 100 : 1st Feb'2025
+ * 
+ * Find out frequency of each word on user defined input. Hint : Use scanner
+ * class and Map.
+ */
+
+public class A100_WordFreqUsingScannerAndMap {
+
+	private static Map<String, Integer> getWordFreq(String str) {
+		String[] words = str.split(" ");
+		Map<String, Integer> wordFreqMap = new LinkedHashMap<String, Integer>();
+		for (String word : words) {
+			wordFreqMap.put(word, wordFreqMap.getOrDefault(word, 0) + 1);
+		}
+		return wordFreqMap;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Enter a string: ");
+		Scanner scanner = new Scanner(System.in);
+		String inputString = scanner.nextLine();
+		scanner.close();
+		Map<String, Integer> wordFreq = A100_WordFreqUsingScannerAndMap.getWordFreq(inputString);
+		System.out.println(wordFreq);
+	}
+}
