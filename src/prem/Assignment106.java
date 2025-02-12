2c6d435f255d6e22872ee88878db61fae7606650
Prem Lata Gupta <premlata.gupta5@gmail.com>
2025-02-12 22:51:45 +0530

    Assignment106-added

--- src/prem/Assignment106.java
+++ src/prem/Assignment106.java
@@ -2,4 +2,24 @@ package prem;
 
 public class Assignment106 {
 
+	public static void main(String[] args) {
+		Assignment106 assign106 = new Assignment106();
+		int num = 10;
+		assign106.printFibonacci(num);
+	}
+
+	private void printFibonacci(int num) {
+		System.out.println("Print fibinacci number " + num);
+
+		int numAt0 = 0;
+		int numAt1 = 1;
+		System.out.print(numAt0 + " " + numAt1 + " ");
+		int nextFibonnacci = 0;
+		for (int i = 2; i < 10; i++) {
+			nextFibonnacci = numAt0 + numAt1;
+			System.out.print(nextFibonnacci + " ");
+			numAt0 = numAt1;
+			numAt1 = nextFibonnacci;
+		}
+	}
 }
