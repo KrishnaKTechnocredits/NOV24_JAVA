2c343e95cb67040bfc9517d5d124fefb13e7e533
Pranita <jijabaihange.com>
2025-02-20 12:11:58 +0530

    Pranita_Assignment61

---
+++ src/pranita/Assignment61.java
@@ -0,0 +1,26 @@
+package pranita;
+
+
+public class Assignment61 {
+	
+	String printString(String name) {
+		String revName ="";
+		for(int j=name.length()-1; j>=0;j--) {
+		revName =revName + name.charAt(j);
+	    }
+		return revName;
+    }
+	
+	void printReverseArrayString(String[] input) {
+		for(int i=0; i<=input.length-1;i++) {
+			String name = printString(input[i]);
+		System.out.println(name);
+		}
+	}
+	
+	public static void main(String[] args){
+	Assignment61 assignment61 = new Assignment61();
+	String[] input= {"tech", "aashvi", "credits"};
+	assignment61.printReverseArrayString(input);	
+	}
+}
