399c7649f3c6425f677abefc9159e4c3ed2bc9fa
Nikitaw-1011 <nikitawani29@gmail.com>
2025-02-28 17:24:44 +0530

    Shift all the zeros at the begining of an array.

---
+++ src/nikita/arraysdemo/Assignment73.java
@@ -0,0 +1,40 @@
+/* Assignment - 73 : 6th Jan'2024
+
+Shift all the zeros at the begining of an array.
+
+int[] arr = {5,0,22,44,0,0,55,0,12,19};
+output : {0,0,0,0,5,22,44,55,12,19}
+*/
+
+package nikita.arraysdemo;
+
+import java.util.Arrays;
+
+public class Assignment73 {
+	
+	int[] getUpdatedArray(int[] arr)
+	{
+		int[] num = new int[arr.length];
+		int j = arr.length-1;
+		
+		for(int i = arr.length-1; i>=0; i--) 
+		{
+			if(arr[i]>0)
+			{
+				num[j] = arr[i];
+				j--;
+			}
+		}
+		
+		return num;
+	}
+
+	public static void main(String[] args) {
+		int[] arr = {5,0,22,44,0,0,55,0,12,19};
+		Assignment73 a73 = new Assignment73();
+		int[] ans = a73.getUpdatedArray(arr);
+		System.out.println(Arrays.toString(ans));
+
+	}
+
+}
